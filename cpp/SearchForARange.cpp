58b3d5ab802df86fbeb24649627636c114348577
falingzhong <faling.zhong@gmail.com>
2014-06-22 10:48:09 +0800

    add two file

---
+++ cpp/SearchForARange.cpp
@@ -0,0 +1,72 @@
+/**
+*   Basic Ideal: Search the most left, and then the most right. Both in Bisearch algorithm.
+*/
+class Solution {
+public:
+    vector<int> searchRange(int A[], int n, int target) {
+        vector<int> res;
+        if(n==0)
+            return res;
+        int left=searchRangeLeft(A,n,target);
+        int right=searchRangeRight(A,n,target);
+        if(left!=-1)
+            return res;
+        res.push_back(left);
+        res.push_back(right);
+        return res;
+    }
+
+    int searchRangeRight(int A[],int n,int target){
+        int l=0;
+        int r=n-1;
+        int m;
+        int res=n;
+        
+        while(l<=r)
+        {
+            m=(l+r)/2;
+            if(A[m]==target){
+                if(m<res)
+                    res=m;
+                r=m-1;
+            }
+            else if(target<A[m])
+            {
+                r=m-1;
+            }
+            else
+            {
+                l=m+1;
+            }
+        }
+        if(res==n)
+            res=-1;
+        return res;
+    }
+
+    int searchRangeLeft(int A[],int n,int target){
+        int l=0;
+        int r=n-1;
+        int m;
+        int res=-1;
+        
+        while(l<=r)
+        {
+            m=(l+r)/2;
+            if(A[m]==target){
+                if(m>res)
+                    res=m;
+                l=m+1;
+            }
+            else if(target<A[m])
+            {
+                r=m-1;
+            }
+            else
+            {
+                l=m+1;
+            }
+        }
+        return res;
+    }
+};
